c8f5f2dda553ecc9e86930eabb996e84977ebe7e
Iliya <mrdonev@gmail.com>
2022-06-13 16:27:53 +0100

    displaying reviews by category

--- NC-GAMES-FE/fe-nc-games/src/Components/Nav.jsx
+++ NC-GAMES-FE/fe-nc-games/src/Components/Nav.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom'
 
 const Nav = ()=>{
     return <section className="Nav">
-        <Link to='/'>Reviews</Link>
+        <Link to='/reviews'>Reviews</Link>
         <Link to='/categories'>Categories</Link>
         <Link to='/users'>Users</Link>
         </section>
